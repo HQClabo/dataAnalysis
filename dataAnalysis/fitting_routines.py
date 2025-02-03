83948b7b5ae2a32d9e0e9e62ba5c8365a311829f
fabian-oppliger <fabianoppliger@bluewin.ch>
2025-02-03 13:04:23 +0100

    added NestedFit function to fit coupled datasets with shared and individual parameters

--- dataAnalysis/fitting_routines.py
+++ dataAnalysis/fitting_routines.py
@@ -11,6 +11,8 @@ from scipy import optimize as opt
 import lmfit
 from resonator_tools import circuit
 from matplotlib import pyplot as plt
+from copy import deepcopy
+import inspect
 import dataAnalysis.plotting_functions as myplt
 
 class ResonatorFitNotch:
@@ -237,3 +239,218 @@ object = CombinedFitModel(x,y1,y2)
     
 
 
+class NestedFit:
+    """
+    A class to perform multi-dataset fitting with shared and individual parameters.
+    It perfors nested fits on multiple datasets with shared parameters and individual parameters.
+    The inner fits are performed with the lmfit.Model class, while the outer fit is performed with the lmfit.Minimizer class.
+
+    """
+
+    def __init__(self, model_func, independent_var_names: list, shared_param_names: list, individual_param_names: list):
+        """
+        Initializes the NestedFit class with the model function and parameter names.
+
+        Parameters:
+        ----------
+        model_func : callable
+            The model function to be fitted to the data.
+        independent_var_names : list
+            List of names of the independent variables.
+        shared_param_names : list
+            List of names of the shared parameters.
+        individual_param_names : list
+            List of names of the individual parameters.
+        """
+        self.model_func = model_func
+        self.independent_var_names = independent_var_names
+        self.shared_param_names = shared_param_names
+        self.individual_param_names = individual_param_names
+        self.result_shared = None
+        self.results_individual = None
+
+    def _generate_params(self, param_names: list, fixed_params: list, guesses: dict, bounds: dict):
+        """
+        Generates lmfit.Parameters for the model function.
+
+        Parameters:
+        ----------
+        param_names : list
+            List of parameter names to be included.
+        fixed_params : list
+            List of parameter names that should be fixed.
+        guesses : dict
+            Dictionary of initial guesses for the parameters.
+        bounds : dict
+            Dictionary of bounds for the parameters.
+
+        Returns:
+        -------
+        lmfit.Parameters
+            The generated parameters for the model function.
+        """
+        signature = inspect.signature(self.model_func)
+        params = lmfit.Parameters()
+        for param in signature.parameters.values():
+            if param.name in param_names:
+                vary = param.name not in fixed_params
+                guess = guesses.get(param.name, 0)
+                min, max = bounds.get(param.name, (-np.inf, np.inf))
+                params.add(param.name, value=guess, min=min, max=max, vary=vary)
+        return params
+
+    def individual_fit(self, params, data, independent_vars: dict, **kwargs):
+        """
+        Performs an individual fit on a single dataset.
+
+        Parameters:
+        ----------
+        params : lmfit.Parameters
+            The parameters for the fit.
+        data : array-like
+            The data to be fitted.
+        independent_vars : dict
+            Dictionary of independent variables.
+
+        Returns:
+        -------
+        lmfit.ModelResult
+            The result of the fit.
+        """
+        model = lmfit.Model(self.model_func, independent_vars=independent_vars.keys())
+        result = model.fit(data, params, **independent_vars, **kwargs)
+        return result
+
+    def nested_fit(self, shared_params: lmfit.Parameters, datasets: list, independent_vars: list,
+                   fixed_params: list, guesses: list, bounds: list, **kwargs):
+        """
+        Performs nested fits on multiple datasets with shared parameters.
+
+        Parameters:
+        ----------
+        shared_params : lmfit.Parameters
+            The shared parameters for the fits.
+        datasets : list
+            List of datasets to be fitted.
+        independent_vars : list
+            List of dictionaries of independent variables for each dataset. The list should have the same length as datasets,
+            and each dictionary should have the same keys as the independent_var_names attribute and contain their respective values.
+        fixed_params : list
+            List of parameter names that should be fixed.
+        guesses : list
+            List of dictionaries of initial guesses for the parameters for each dataset.
+        bounds : list
+            List of dictionaries of bounds for the parameters for each dataset.
+
+        Returns:
+        -------
+        list
+            List of lmfit.ModelResult objects for each dataset.
+        """
+        results = []
+        for i, data in enumerate(datasets):
+            params = self._generate_params(self.individual_param_names, fixed_params, guesses[i], bounds[i])
+            params.update(deepcopy(shared_params))
+            for param in params and shared_params:
+                params[param].vary = False
+            result = self.individual_fit(params, data, independent_vars[i], **kwargs)
+            results.append(result)
+        return results
+
+    def residuals_nested_fit(self, shared_params: lmfit.Parameters, datasets: list, independent_vars: list,
+                             fixed_params: list, guesses: dict, bounds: dict, **kwargs):
+        """
+        Performs the nested fits on all datasets and calculates their residuals as a single 1d numpy.array.
+
+        Parameters:
+        ----------
+        shared_params : lmfit.Parameters
+            The shared parameters for the fits.
+        datasets : list
+            List of datasets to be fitted.
+        independent_vars : list
+            List of dictionaries of independent variables for each dataset. The list should have the same length as datasets,
+            and each dictionary should have the same keys as the independent_var_names attribute and contain their respective values.
+        fixed_params : list
+            List of parameter names that should be fixed.
+        guesses : dict
+            Dictionary of initial guesses for the parameters.
+        bounds : dict
+            Dictionary of bounds for the parameters.
+
+        Returns:
+        -------
+        np.array
+            Array of residuals from the fits.
+        """
+        results = self.nested_fit(shared_params, datasets, independent_vars, fixed_params, guesses, bounds, **kwargs)
+        residuals = np.array([])
+        for result in results:
+            residuals = np.append(residuals, result.residual.flatten())
+        return residuals
+
+    def fit(self, datasets: list, independent_vars: list, fixed_params: list,
+                  guesses_shared: dict, bounds_shared: dict, guesses_individual: list, bounds_individual: list,
+                  indiv_kws: dict=dict(), global_kws: dict=dict(), **kwargs):
+        """
+        Performs a global fit across multiple datasets with shared and individual parameters.
+
+        Parameters:
+        ----------
+        datasets : list
+            List of datasets to be fitted.
+        independent_vars : list
+            List of dictionaries of independent variables for each dataset. The list should have the same length as datasets,
+            and each dictionary should have the same keys as the independent_var_names attribute and contain their respective values.
+        fixed_params : list
+            List of parameter names that should be fixed.
+        guesses_shared : dict
+            Dictionary of initial guesses for the shared parameters.
+        bounds_shared : dict
+            Dictionary of bounds for the shared parameters.
+        guesses_individual : list
+            List of dictionaries of initial guesses for the individual parameters for each dataset.
+        bounds_individual : list
+            List of dictionaries of bounds for the individual parameters for each dataset.
+        indiv_kws : dict, optional
+            Additional keyword arguments for individual fits.
+        global_kws : dict, optional
+            Additional keyword arguments for the global fit.
+
+        Returns:
+        -------
+        tuple
+            The result of the shared fit and a list of results for the individual fits.
+        """
+        shared_params = self._generate_params(self.shared_param_names, fixed_params, guesses_shared, bounds_shared)
+        minimizer = lmfit.Minimizer(self.residuals_nested_fit, shared_params,
+                                    fcn_args=(datasets, independent_vars, fixed_params, guesses_individual, bounds_individual),
+                                    fcn_kws=indiv_kws, **kwargs)
+        result_shared = minimizer.minimize(params=shared_params, **global_kws)
+        results_individual = self.nested_fit(shared_params=result_shared.params,
+                                             datasets=datasets, independent_vars=independent_vars, fixed_params=fixed_params,
+                                             guesses=guesses_individual, bounds=bounds_individual, **indiv_kws)
+        self.result_shared = result_shared
+        self.results_individual = results_individual
+        return result_shared, results_individual
+
+    def print_fit_report(self, label, label_individual=None):
+        if (self.result_shared is not None) and (self.results_individual is not None):
+            print('')
+            print('Fitted with NestedFit - ' + label)
+            print('')
+            print('######## Global fit ########')
+            print('')
+            print(lmfit.fit_report(self.result_shared))
+            print('')
+            print('######## Individual fits ########')
+            print('')
+            for i, result in enumerate(self.results_individual):
+                if label_individual is not None:
+                    print('### '+label_individual[i]+' ###')
+                else:
+                    print('### Fit '+str(i)+' ###')
+                print(lmfit.fit_report(result))
+                print('')
+        else:
+            print('No fit results available. Run the fit method first.')
